11602fbacae99b064ff17082e5b8f6e8e1279a60
BeokBeok <kekemusa37@nate.com>
2020-03-10 22:23:26 +0900

    [적용] groovy to kts

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,42 @@
+object Version {
+    const val kotlin = "1.3.70"
+}
+
+object AndroidSettings {
+    const val compileSdk = 29
+    const val buildToolsVer = "29.0.3"
+
+    const val minSdk = 29
+    const val targetSdk = 29
+    const val versionCode = 1
+    const val versionName = "1.0.0"
+
+    const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+}
+
+object BasicDep {
+    val fileTreeMap = mapOf("dir" to "libs", "include" to listOf("*.jar"))
+    const val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:${Version.kotlin}"
+}
+
+object UiDep {
+    private const val materialVer = "1.1.0"
+    private const val constraintLayoutVer = "1.1.3"
+
+    const val material = "com.google.android.material:material:$materialVer"
+    const val constraintLayout = "androidx.constraintlayout:constraintlayout:$constraintLayoutVer"
+}
+
+object TestDep {
+    private const val junitVer = "4.12"
+
+    const val junit = "junit:junit:$junitVer"
+}
+
+object AndroidTestDep {
+    private const val junitExtVer = "1.1.1"
+    private const val espressoVer = "3.2.0"
+
+    const val junitExt = "androidx.test.ext:junit:$junitExtVer"
+    const val espressoCore = "androidx.test.espresso:espresso-core:$espressoVer"
+}
