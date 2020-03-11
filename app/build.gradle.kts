7533a5a20c142bbc22d67ed413183c4dc51506f5
BeokBeok <kekemusa37@nate.com>
2020-03-12 00:26:14 +0900

    [적용] precompile 적용
    - 단점
    1. 멀티 모듈에서 사용 부적합
    2. buildSrc 단 gradle 에서 Dependencies.kt 접근 불가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,41 +1,15 @@
 plugins {
-    id("com.android.application")
-    id("kotlin-android")
-    id("kotlin-android-extensions")
-    id("kotlin-kapt")
+    id("common-android")
 }
 
 android {
-    compileSdkVersion(AndroidSettings.compileSdk)
     defaultConfig {
         applicationId = "com.example.android10spec"
-        minSdkVersion(AndroidSettings.minSdk)
-        targetSdkVersion(AndroidSettings.targetSdk)
-        versionCode = AndroidSettings.versionCode
-        versionName = AndroidSettings.versionName
-
         testInstrumentationRunner = AndroidSettings.testInstrumentationRunner
-    }
-
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = true
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-
-    dataBinding {
-        isEnabled = true
     }
 }
 
 dependencies {
-    implementation(fileTree(BasicDep.fileTreeMap))
-    implementation(BasicDep.kotlin)
-
     UiDep.run {
         implementation(material)
         implementation(constraintLayout)
--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,7 +1,16 @@
 repositories {
     jcenter()
+    google()
 }
 
 plugins {
     `kotlin-dsl`
+    `kotlin-dsl-precompiled-script-plugins`
+}
+
+// Dependencies.kt 에 접근 불가
+dependencies {
+    implementation("com.android.tools.build:gradle:3.6.1")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.3.70")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.70")
 }
