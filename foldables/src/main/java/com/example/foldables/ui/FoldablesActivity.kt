8b90ae1aba9052a0df77690847f47f31929a0854
BeokBeok <kekemusa37@nate.com>
2020-04-23 23:43:30 +0900

    [적용] foldables 관련 하이퍼링크 적용

--- foldables/src/main/java/com/example/foldables/ui/FoldablesActivity.kt
+++ foldables/src/main/java/com/example/foldables/ui/FoldablesActivity.kt
@@ -2,6 +2,8 @@ package com.example.foldables.ui
 
 import android.content.Context
 import android.content.Intent
+import android.os.Bundle
+import android.text.method.LinkMovementMethod
 import com.example.common.base.BaseActivity
 import com.example.foldables.R
 import com.example.foldables.databinding.ActivityFoldablesBinding
@@ -11,6 +13,15 @@ class FoldablesActivity : BaseActivity<ActivityFoldablesBinding>(R.layout.activi
 
     override fun setupInject() {
         (application as FoldablesComponentProvider).getFoldablesComponent().inject(this)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setupUi()
+    }
+
+    private fun setupUi() {
+        binding.tvContents.movementMethod = LinkMovementMethod.getInstance()
     }
 
     companion object {
